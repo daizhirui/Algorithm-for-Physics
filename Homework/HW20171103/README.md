e0317de28680ff4441f3a657a863bbda9e2d9160
daizhirui <daizhirui@hotmail.com>
2017-11-29 15:09:29 +0800

    HW20171103 MyVector, a vector calculation class

---
+++ Homework/HW20171103/README.md
@@ -0,0 +1,109 @@
+# MyVector
+
+## Introduction
+MyVector is a math class supporting the basic calculation of vector.
+The class is achieved in ___MyVector.cpp___ and ___MyVector.h___
+The test program is achieved in ___testMyVector.cpp___, which shows some examples of using this class.
+
+## Compile and test
+
+```bash
+make            # compile MyVector as a static library and compile testMyVector
+make lib        # compile MyVector as a static library
+make test       # run testMyVector to test this class
+make clean      # clean *.a *.o files
+make cleanall   # clean *.a *.o and the exec bin
+```
+
+## History
+- 2017-11-03
+    - first release, basic functions completed
+- 2017-11-10
+    - add operator[], dot_plus, cross_plus
+- 2017-11-17
+    - add angleBetween, dist, random_vector
+- 2017-11-18
+    - add operator==, operator!=, operator-(negative sign)
+- 2017-11-19
+    - try to add rotate functions, common usage unknown
+- 2017-11-20
+    - stop the development of rotate functions
+    - complete the test program
+- 2017-11-27
+    - update the makefile
+    - project completed, upload to the server
+
+## Member functions
+
+### Construct functions
+
+```cpp
+MyVector::MyVector();
+MyVector::MyVector(int);
+MyVector::MyVector(int, const vector<double>&);
+MyVector::MyVector(int, const double*);
+MyVector::MyVector(const MyVector&);
+```
+### Deconstruct function
+
+```cpp
+MyVector::~MyVector();
+```
+### Dimension setup functions
+
+```cpp
+void MyVector::set_dimension(int);
+int MyVector::dimension();
+```
+### Position setup functions
+
+```cpp
+void MyVector::set_position(const vector<double>&);
+void MyVector::set_position(const double*);
+```
+
+### operator
+
+```cpp
+double& MyVector::operator[](int);
+double MyVector::operator[](int) const;
+void MyVector::operator=(const MyVector&);
+MyVector MyVector::operator+(const MyVector&) const;
+MyVector MyVector::operator-(const MyVector&) const;
+MyVector MyVector::operator-() const;
+MyVector MyVector::operator*(const double&) const;
+friend MyVector operator*(const double&, const MyVector&);
+MyVector MyVector::operator/(const double&);
+MyVector MyVector::operator+=(const MyVector&);
+MyVector MyVector::operator-=(const MyVector&);
+MyVector MyVector::operator*=(const double&);
+MyVector MyVector::operator/=(const double&);
+friend ostream& operator<<(ostream&, const MyVector&);
+bool MyVector::operator==(const MyVector&) const;
+bool MyVector::operator!=(const MyVector&) const;
+```
+
+### Calculation functions
+
+```cpp
+MyVector MyVector::normalize() const;
+void MyVector::normalized();
+double MyVector::modulus() const;
+void MyVector::set_modulus(const double&);
+double MyVector::dot_plus(const MyVector&) const;
+static double MyVector::dot_plus(const MyVector&, const MyVector&);
+MyVector MyVector::cross_plus(const MyVector&) const;
+static MyVector::MyVector cross_plus(const MyVector&, const MyVector&);
+double MyVector::angleBetween(const MyVector&) const;
+static double MyVector::angleBetween(const MyVector&, const MyVector&);
+double MyVector::dist(const MyVector&) const;
+static MyVector::double dist(const MyVector&, const MyVector&);
+```
+
+### Other functions
+
+```cpp
+void MyVector::random_vector();
+static MyVector MyVector::random_vector(int);
+```
+
---
+++ Homework/HW20171103/testMyVector.cpp
@@ -0,0 +1,186 @@
+//
+//  testMyVector.cpp
+//
+//  Created by 戴植锐 on 2017/11/03.
+//  Copyright © 2017年 戴植锐. All rights reserved.
+//
+#include <iostream>
+#include <vector>
+#include "MyVector.h"
+using namespace std;
+void printInfo(const char* str)
+{
+    cout << str << endl;
+}
+void printInfo(const char* str, const double& v)
+{
+    cout << str << v << endl;
+}
+void printInfo(const char* str, const MyVector& v)
+{
+    cout << str << v << endl;
+}
+int main()
+{   
+    // Test Construct Functions
+    printInfo("###### Test Construct Functions ######");
+    printInfo("operator<< is also tested when MyVector instance is printed");
+    printInfo("void MyVector::set_dimension(int) is also tested");
+    printInfo("Test MyVector::MyVector() and int MyVector::dimension()");
+    MyVector vector1 = MyVector();
+    printInfo("_nDim= ",vector1.dimension());
+    printInfo("vector1= ",vector1);
+
+    printInfo("Test MyVector::MyVector(int)");
+    MyVector vector2(1);
+    printInfo("_nDim= ",vector2.dimension());
+    printInfo("vector2= ",vector2);
+
+    printInfo("Test MyVector::MyVector(int,vector)");
+    double num[] = {1,0,1,0};
+    vector<double> vector0(num,num+4);
+    MyVector vector3(4,vector0);
+    printInfo("_nDim= ",vector3.dimension());
+    printInfo("vector3= ",vector3);
+
+    printInfo("Test MyVector::MyVector(int,double)");
+    double pos[]={1,2,3,4,5};
+    MyVector vector4(sizeof(pos)/sizeof(double),pos);
+    printInfo("_nDim= ",vector4.dimension());
+    printInfo("vector4= ",vector4);
+
+    printInfo("Test MyVector::MyVector(MyVector)\n and operator= (inside this Construct Function)");
+    MyVector vector5(vector4);
+    printInfo("_nDim= ",vector5.dimension());
+    printInfo("vector5= ",vector5);
+    printInfo("");
+
+    // Test Member value set-function and get-function
+    printInfo("Test void MyVector::set_position");
+    printInfo("Before set_position(vector0), vector1._nDim= ",vector1.dimension());
+    printInfo("Before set_position(vector0), vector1= ",vector1);
+    vector1.set_position(vector0);
+    printInfo("After set_position(vector0), vector1._nDim= ",vector1.dimension());
+    printInfo("After set_position(vector0), vector1= ",vector1);
+    printInfo("");
+
+    // Test operators
+    printInfo("###### Test operators ######");
+    printInfo("Test double& MyVector::operator[](int) or double MyVector::operator[](int) const");
+    cout << "vector1[0]= " << vector1[0] << endl;
+    vector1.set_dimension(3);
+    vector0.resize(3);
+    for(int i=0;i<3;i++)
+        vector0[i] = i;
+    vector1.set_position(vector0);  // vector1 = (0, 1, 2)
+    vector2.set_dimension(3);
+    for(int i=1;i<4;i++)
+        vector0[i-1] = i;
+    vector2.set_position(vector0);  // vector2 = (1, 2, 3)
+    printInfo("Test operator+ - * /");
+    printInfo("\t vector1 = ",vector1);
+    printInfo("\t vector2 = ",vector2);
+    printInfo("vector1 + vector2 = ",vector1 + vector2);
+    printInfo("vector1 - vector2 = ",vector1 - vector2);
+    printInfo("vector1 * 3 = ", vector1 * 3);
+    printInfo("3 * vector1 = ", 3 * vector1);
+    printInfo("vector1 / 3 = ", vector1 / 3);
+    printInfo("-vector1 = ", -vector1);
+    vector1 += vector2;
+    printInfo("vector1 += vector2, vector1 = ", vector1);
+    vector1 -= vector2;
+    printInfo("vector1 -= vector2, vector1 = ", vector1);
+    vector1 *= 5;
+    printInfo("vector1 *= 5, vector1 = ", vector1);
+    vector1 /= 5;
+    printInfo("vector1 /= 5, vector1 = ", vector1);
+    if(vector1 == vector2)
+        printInfo("vector1 == vector 2: true");
+    else
+        printInfo("vector1 == vector 2: false");
+    if(vector1 == vector1)
+        printInfo("vector1 == vector 1: true");
+    else
+        printInfo("vector1 == vector 1: false");
+    if(vector1 != vector2)
+        printInfo("vector1 == vector 2: true");
+    else
+        printInfo("vector1 == vector 2: false");
+    if(vector1 != vector1)
+        printInfo("vector1 == vector 1: true");
+    else
+        printInfo("vector1 == vector 1: false");
+    printInfo("");
+    // Test normalize(d) modulus and set_modulus
+    vector0[0] = 1;
+    vector0[1] = 1;
+    vector0[2] = 0;
+    vector1.set_position(vector0);
+    printInfo("###### Test normalize(d) modulus and set_modulus ######");
+    printInfo("Before vector1.normalize(), vector1 = ", vector1);
+    printInfo("vector1.normalize() = ", vector1.normalize());
+    printInfo("After vector1.normalize(), vector1 = ", vector1);
+    printInfo("Before vector1.normalized(), vector1 = ", vector1);
+    vector1.normalized();
+    printInfo("vector1.normalized() = ", vector1);
+    printInfo("After vector1.normalized(), vector1 = ", vector1);
+    printInfo("vector1.modulus() = ",vector1.modulus());
+    vector1.set_modulus(2);
+    printInfo("After vector1.set_modulus(2), vector1 = ",vector1);
+    printInfo("vector1.modulus() = ",vector1.modulus());
+    printInfo("");
+
+    //Test dot_plus cross_plus
+    printInfo("###### Test dot_plus and cross_plus ######");
+    vector1.set_position(vector0);
+    printInfo("\t vector1 = ",vector1);
+    printInfo("\t vector2 = ",vector2);
+    printInfo("vector1.dot_plus(vector2) = ",vector1.dot_plus(vector2));
+    printInfo("vector2.dot_plus(vector1) = ",vector2.dot_plus(vector1));
+    printInfo("vector1.dot_plus(vector1) = ",vector1.dot_plus(vector1));
+    printInfo("dot_plus(vector1, vector2) = ", MyVector::dot_plus(vector1, vector2));
+    printInfo("dot_plus(vector2, vector1) = ", MyVector::dot_plus(vector2, vector1));
+    printInfo("dot_plus(vector1, vector1) = ", MyVector::dot_plus(vector1, vector1));
+    printInfo("vector1.cross_plus(vector2) = ",vector1.cross_plus(vector2));
+    printInfo("vector2.cross_plus(vector1) = ",vector2.cross_plus(vector1));
+    printInfo("vector1.cross_plus(vector1) = ",vector1.cross_plus(vector1));
+    printInfo("cross_plus(vector1, vector2) = ", MyVector::cross_plus(vector1, vector2));
+    printInfo("cross_plus(vector2, vector1) = ", MyVector::cross_plus(vector2, vector1));
+    printInfo("cross_plus(vector1, vector1) = ", MyVector::cross_plus(vector1, vector1));
+    printInfo("");
+
+    //Test angleBetween dist
+    printInfo("###### Test angleBetween and dist ######");
+    printInfo("\t vector1 = ",vector1);
+    printInfo("\t vector2 = ",vector2);
+    vector3 = vector1.cross_plus(vector2);
+    printInfo("\t vector3 = vector1 x vector2 ",vector3);
+    printInfo("vector1.angleBetween(vector3) = ", vector1.angleBetween(vector3));
+    printInfo("vector3.angleBetween(vector1) = ", vector3.angleBetween(vector1));
+    printInfo("angleBetween(vector1, vector3) = ", MyVector::angleBetween(vector1, vector3));
+    printInfo("angleBetween(vector3, vector1) = ", MyVector::angleBetween(vector3, vector1));
+    printInfo("\t vector1 = ",vector1);
+    printInfo("\t vector2 = ",vector2);
+    printInfo("\t vector3 = vector1 x vector2 ",vector3);
+    printInfo("vector1.dist(vector2) = ", vector1.dist(vector2));
+    printInfo("vector2.dist(vector1) = ", vector2.dist(vector1));
+    printInfo("dist(vector1, vector2) = ", MyVector::dist(vector1, vector2));
+    printInfo("dist(vector2, vector1) = ", MyVector::dist(vector2, vector1));
+    printInfo("");
+
+    //Test random_vector
+    printInfo("###### Test random_vector ######");
+    printInfo("Before vector1.random_vector(), vector1 = ", vector1);
+    vector1.random_vector();
+    printInfo("vector1.random_vector() = ", vector1);
+    printInfo("After vector1.random_vector(), vector1 = ", vector1);
+    printInfo("Now we generate a new vector randomly and give it to vector5");
+    printInfo("Before that, vector5 = ", vector5);
+    vector5 = MyVector::random_vector(4);
+    printInfo("vector5 = MyVector::random_vector(4) = ", vector5);
+    printInfo("vector5.modulus() = ", vector5.modulus());
+    printInfo("");
+    
+    printInfo("******** Congratulations! Test Completed! ********");
+    return 0;
+}
